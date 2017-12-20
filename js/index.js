1700887ed151f49e71938ab196d3c17d450ebf20
Dita <Ditiite@users.noreply.github.com>
2017-12-20 14:44:30 +0200

    Add images

---
+++ js/index.js
@@ -0,0 +1,11 @@
+
+var linkWeb = $('#web');
+var linkDesign = $('#design');
+var webText = $('#web-text');
+var dText = $('#design-text');
+
+$('button').on('click', function () {
+    
+      linkWeb.toggleClass('hidden');
+    
+    });
